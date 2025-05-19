7453cc3afbb39681d4b82327c689f1f2952fa394
kiruke <kiruke2525@gmail.com>
2025-05-19 19:17:52 +0900

    タイムゾーン東京設定

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,8 @@ module RunteqNormal
     config.i18n.available_locales = %i[ja en]
     config.i18n.default_locale = :ja # 追加 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.time_zone = 'Tokyo'
   end
 end
 

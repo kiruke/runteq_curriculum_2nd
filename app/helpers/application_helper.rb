defd9700d9da4cda4e8c35e7764cec0be01d3699
kiruke <kiruke2525@gmail.com>
2025-06-15 02:15:16 +0900

    content_forで動的タイトル出力

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def page_title
+    if content_for?(:title)
+      content_for(:title) + " | " + "RUNTEQ BOARD APP"
+    else
+      "RUNTEQ BOARD APP" 
+    end
+  end
 end

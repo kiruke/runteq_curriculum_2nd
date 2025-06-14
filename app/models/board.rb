c248e3b898194f6e0cd1bcbae5a9d1832946dc8a
kiruke <kiruke2525@gmail.com>
2025-06-15 04:18:03 +0900

    自分が作成した掲示板のみ、編集・削除ボタンが表示されるよう追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -6,4 +6,8 @@ class Board < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65535 }
+
+  def is_mine?(current_user)
+    self.user == current_user
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,18 +8,4 @@ class Comment < ApplicationRecord
     self.user == current_user
   end
   
-    # 今回確認用に新しく追加するメソッド
-  def who_am_i
-    puts "私は誰でしょう？: #{self}"
-    puts "私のクラスは？: #{self.class}"
-    puts "私のIDは？: #{self.id}" # ApplicationRecordを継承している場合
-    puts "私のユーザーは？: #{self.user.decorate.full_name}" # userがある場合
-    self # 最後に自分自身を返すこともできます
-  end
-
-    # デバッグ用のメソッド
-  def show_my_self
-    puts "メソッド内部の self は: #{self.inspect}"
-    puts "メソッド内部の self のクラスは: #{self.class}"
-  end
 end

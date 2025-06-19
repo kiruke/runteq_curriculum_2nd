0f274e14e352a11b2392590b725c868b52e6f979
kiruke <kiruke2525@gmail.com>
2025-06-20 07:45:23 +0900

    ブックマーク一覧機能追加

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,19 +2,22 @@ class BookmarksController < ApplicationController
   def create
     bookmark = current_user.bookmarks.build(board_id: params[:board_id])
     if bookmark.save
-      redirect_to boards_path, notice: ('bookmarkしました')
+      redirect_to boards_path, success: ('ブックマークしました')
     else
-      redirect_back_or_to boards_path, alert: 'bookmarkに失敗しました'
+      redirect_back_or_to boards_path, alert: 'ブックマークに失敗しました'
     end
   end
 
   def destroy
     bookmark = current_user.bookmarks.find_by(id: params[:id])
     bookmark.destroy!
-    redirect_to boards_path, notice: ('bookmarkを削除しました')
+    redirect_to boards_path, success: ('ブックマークを外しました')
   end
 
   def index
+    #@bookmarks = current_user.bookmarks N+1問題発生
+    #@bookmarks = current_user.bookmarks.includes(:board)
+    @bookmarks = current_user.bookmarks.includes(board: :user)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create] do
+    resources :bookmarks, only: [:index], shallow: true
+  end
   resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create, :destroy], shallow: true
-    resources :bookmarks, only: [:index, :create, :destroy], shallow: true
+    resources :bookmarks, only: [:create, :destroy], shallow: true
   end
 end

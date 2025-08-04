b8c22ef44fd9a99786962ec7dedf8329fb51eb57
kiruke <kiruke2525@gmail.com>
2025-08-04 22:14:20 +0900

    bookmark一覧のルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  resources :users, only: [:new, :create] do
-    resources :bookmarks, only: [:index], shallow: true
-  end
+  resources :users, only: [:new, :create]
   resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create], shallow: true
     collection do

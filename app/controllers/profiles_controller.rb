4d47de42b86d66a8b74def77c81dc67f8e5bc6cd
kiruke <kiruke2525@gmail.com>
2025-08-25 23:14:04 +0900

    プロフィール名の変更に失敗した際に、画面上でプロフィールとして表示される名前が変更されるため修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,11 +8,21 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    if @profile.update(profile_params)
+      redirect_to profile_path, success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: User.model_name.human)
+      render :edit
+    end 
   end
 
   private
 
   def set_profile
-    @profile = current_user  
+    @profile = User.find(current_user.id)  
+  end
+
+  def profile_params
+    params.require(:user).permit(:email, :first_name, :last_name, :avatar, :avatar_cache)
   end
 end
